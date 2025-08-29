b98bc35437e48b658583f7fb783280a3c836f7ab
hariprasath211510 <hariprasath10072002@gmail.com>
2025-08-29 06:27:08 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Array;
+public class array{
+ public static void main(String args[]){
+    int arr[]={18,7,9,19};
+    int maxval=Integer.MIn_VALUE;
+    for(int i=0;i<arr.length;i++){
+     if(arr[i]>maxValue){
+       maxValue=arr[i];
+      }
+}
